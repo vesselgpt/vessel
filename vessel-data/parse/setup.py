309a288bcc4649132e24bac9bfeef3bb6cf4fbd2
dldiego1 <honeygithub12@outlook.com>
2024-07-18 10:39:15 +0300

    Removed Poetry

---
+++ vessel-data/parse/setup.py
@@ -0,0 +1,37 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+with open("requirements.txt", "r", encoding="utf-8") as fh:
+    requirements = fh.read().splitlines()
+
+setup(
+    name="vessel-parse",
+    version="0.3.2",
+    author="Andrej Baranovskij",
+    author_email="andrejus.baranovskis@gmail.com",
+    description="Vessel Parse is a Python package for parsing and extracting information from documents.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/vesselgpt/vessel/tree/main/vessel-data/parse",
+    project_urls={
+        "Homepage": "https://github.com/vesselgpt/vessel/tree/main/vessel-data/parse",
+        "Repository": "https://github.com/vesselgpt/vessel",
+    },
+    classifiers=[
+        "Operating System :: OS Independent",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Topic :: Software Development",
+        "Programming Language :: Python :: 3.10",
+    ],
+    entry_points={
+        'console_scripts': [
+            'vessel-parse=vessel_parse:main',
+        ],
+    },
+    keywords="llm, rag, vision",
+    packages=find_packages(),
+    python_requires='>=3.10',
+    install_requires=requirements,
+)
